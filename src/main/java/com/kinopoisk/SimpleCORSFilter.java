351672fe72dd915bacc4c57ff73978038f7beae3
m.shkurko <5611771@mail.ru>
2017-12-18 14:20:16 +0300

    add to crosfilter 'Access-Control-Allow-Origin'

--- src/main/java/com/kinopoisk/SimpleCORSFilter.java
+++ src/main/java/com/kinopoisk/SimpleCORSFilter.java
@@ -23,7 +23,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin");
         chain.doFilter(req, res);
     }
 
